2149383526c823857f9beba3184d0abeff4576b5
Ekaterina Tyurina <etyurina@twitter.com>
2019-08-06 15:34:38 +0100

    Changes in zipkin reporter related to concrete time in rust

--- src/python/pants/reporting/zipkin_reporter.py
+++ src/python/pants/reporting/zipkin_reporter.py
@@ -15,6 +15,7 @@ from pants.reporting.reporter import Reporter
 
 logger = logging.getLogger(__name__)
 
+NUM_NANOSECS_IN_SEC = 1000000000.0
 
 class HTTPTransportHandler(BaseTransportHandler):
   def __init__(self, endpoint):
@@ -156,7 +157,14 @@ class ZipkinReporter(Reporter):
   def bulk_record_workunits(self, engine_workunits):
     """A collection of workunits from v2 engine part"""
     for workunit in engine_workunits:
-      duration = workunit['end_timestamp'] - workunit['start_timestamp']
+      start_timestamp = from_secs_and_nanos_to_float(
+        workunit['start_secs'],
+        workunit['start_nanos']
+      )
+      duration = from_secs_and_nanos_to_float(
+        workunit['duration_secs'],
+        workunit['duration_nanos']
+      )
 
       local_tracer = get_default_tracer()
 
@@ -176,5 +184,8 @@ class ZipkinReporter(Reporter):
         flags='0', # flags: stores flags header. Currently unused
         is_sampled=True,
       )
-      span.start_timestamp = workunit['start_timestamp']
+      span.start_timestamp = start_timestamp
       span.stop()
+
+def from_secs_and_nanos_to_float(secs, nanos):
+  return secs + nanos/NUM_NANOSECS_IN_SEC
